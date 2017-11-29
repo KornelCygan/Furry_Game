ff0280c84c254b67b7966644d5dff35fd0e916ab
KornelCygan <kornel.cygan@gmail.com>
2017-11-29 15:08:03 +0100

    Divided for modules, Furry stearing and coins colect

--- js/out.js
+++ js/out.js
@@ -70,92 +70,155 @@
 "use strict";
 
 
-console.log('hello');
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM fully loaded and parsed!");
 
-function Furry() {
-    this.x = 0;
-    this.y = 0;
-    this.direction = 'right';
-}
+    var Game = __webpack_require__(1);
 
-function Coin() {
-    this.x = Math.floor(Math.random() * 10);
-    this.y = Math.floor(Math.random() * 10);
-    console.log(this.x, this.y);
-}
+    var game1 = new Game();
+    game1.show_furry();
+    game1.show_coin();
+    game1.start_game();
+    document.addEventListener('keydown', function (event) {
+        game1.furryControlled(event);
+    });
+});
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var Furry = __webpack_require__(2);
+var Coin = __webpack_require__(3);
 
 function Game() {
-    this.board = document.querySelectorAll('#board > div');
-
     this.furry = new Furry();
     this.coin = new Coin();
+    this.board = document.querySelectorAll("#board>div");
     this.score = 0;
-
     this.index = function (x, y) {
         return x + y * 10;
     };
 
-    this.showFurry = function () {
+    this.show_furry = function () {
+        this.hideVisibleFurry();
         this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
-        console.log(this.furry.x, this.furry.y);
     };
-
-    this.hideVisibleFurry = function () {
-        var divFurry = document.querySelector('.furry');
-        divFurry.classList.remove('furry');
-        console.log(divFurry + "furry to ja");
-    };
-
-    this.showCoin = function () {
+    this.show_coin = function () {
+        console.log(this.coin);
         this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
     };
 
-    var self = this;
+    this.move_furry = function () {
 
-    this.startGame = function () {
-        this.idSetInterval = setInterval(function () {
-            self.moveFurry();
-        }, 1000);
-    };
-
-    this.moveFurry = function () {
-
-        if (self.furry.direction === "right") {
-            self.furry.x = self.furry.x + 1;
-        } else if (self.furry.direction === "left") {
-            self.furry.x = self.furry.x - 1;
-        } else if (self.furry.direction === "up") {
-            self.furry.y = self.furry.y + 1;
-        } else if (self.furry.direction === "down") {
-            self.furry.y = self.furry.y - 1;
+        if (this.furry.direction === "up") {
+            this.furry.y -= 1;
+        } else if (this.furry.direction === "right") {
+            this.furry.x += 1;
+        } else if (this.furry.direction === "down") {
+            this.furry.y += 1;
+        } else if (this.furry.direction === "left") {
+            this.furry.x -= 1;
         }
-        self.showFurry();
-        this.hideVisibleFurry();
+        this.gameOver();
+        this.checkCoinCollision();
+    };
+    this.hideVisibleFurry = function () {
+        var old_furry = document.querySelector('.furry');
+        if (old_furry !== null) {
+            //bo inaczej pojawial się błąd 'cannot read classlist of null'
+            old_furry.classList.remove('furry');
+        }
     };
 
-    this.turnFurry = function (event) {
+    this.furryControlled = function (event) {
         switch (event.which) {
             case 37:
                 this.furry.direction = 'left';
                 break;
             case 38:
-                this.furry.direction = 'right';
+                this.furry.direction = 'up';
                 break;
             case 39:
-                this.furry.direction = 'up';
+                this.furry.direction = 'right';
                 break;
             case 40:
                 this.furry.direction = 'down';
+                break;
         }
     };
-    document.addEventListener('keydown', this.turnFurry);
+
+    this.checkCoinCollision = function () {
+        if (this.furry.x === this.coin.x && this.furry.y === this.coin.y) {
+            var coin = document.querySelector('.coin');
+            coin.classList.remove('coin');
+
+            this.score += 1;
+            var score_info = document.querySelector('strong');
+            score_info.innerText = this.score;
+
+            this.coin = new Coin();
+            this.show_coin();
+        }
+    };
+
+    this.gameOver = function () {
+        if (this.furry.x < 0 || this.furry.x > 9 || this.furry.y < 0 || this.furry.y > 9) {
+            clearInterval(this.idSetInterval);
+
+            var over = document.getElementById('over');
+            over.classList.remove('invisible');
+            var game_over_info = document.querySelector('h3');
+            var newSpan = document.createElement('span');
+            newSpan.innerText = " Your score: " + this.score;
+            game_over_info.appendChild(newSpan);
+            game_over_info.classList.remove('invisible');
+            console.log('game over!');
+        }
+    };
+
+    var self = this;
+    this.start_game = function startGame() {
+        this.idSetInterval = setInterval(function () {
+            self.move_furry();
+            self.show_furry();
+        }, 250);
+    };
 }
 
-var play = new Game();
-play.showFurry();
-play.showCoin();
-play.startGame();
-play.startGame();
+module.exports = Game;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+function Furry() {
+   this.x = 0;
+   this.y = 0;
+   this.direction = "right";
+}
+
+module.exports = Furry;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+function Coin() {
+
+    this.x = Math.floor(Math.random() * 10);
+    this.y = Math.floor(Math.random() * 10);
+}
+module.exports = Coin;
 
 /***/ })
 /******/ ]);
